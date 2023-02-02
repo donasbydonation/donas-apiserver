f6451c2e2040ab86d0a3c59c3dd069ea9fc2fbd6
SeoSeunghun <proto_seo@naver.com>
2023-02-02 16:22:45 +0900

    [init] Boost 프로젝트 초기화

---
+++ src/main/java/me/donas/boost/BoostApplication.java
@@ -0,0 +1,13 @@
+package me.donas.boost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/donas/boost/BoostApplicationTests.java
@@ -0,0 +1,13 @@
+package me.donas.boost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
