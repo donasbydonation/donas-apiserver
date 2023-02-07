74fce57ea5535988696685e33dd08aa96e2ea5e6
SeoSeunghun <proto_seo@naver.com>
2023-02-07 17:45:22 +0900

    [feat][#1] email validation 추가

--- src/main/java/me/donas/boost/domain/preregistration/dto/PreRegistrationRequest.java
+++ src/main/java/me/donas/boost/domain/preregistration/dto/PreRegistrationRequest.java
@@ -4,9 +4,9 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import me.donas.boost.domain.preregistration.domain.PreRegistration;
 
-public record PreRegistrationDto(
-	@NotEmpty(message = "이메일 형식을 확인해주세요.")
-	@Email(message = "이메일 형식을 확인해주세요.")
+public record PreRegistrationRequest(
+	@NotEmpty
+	@Email
 	String email
 ) {
 	public PreRegistration toEntity() {
