46cfbcd5e5353364f6e24fb81a2fa2235f474d15
SeoSeunghun <proto_seo@naver.com>
2023-02-07 17:44:48 +0900

    [feat][#1] DTO validation 추가

--- src/main/java/me/donas/boost/domain/preregistration/api/PreRegistrationController.java
+++ src/main/java/me/donas/boost/domain/preregistration/api/PreRegistrationController.java
@@ -3,17 +3,17 @@ package me.donas.boost.domain.preregistration.api;
 import java.net.URI;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import me.donas.boost.domain.preregistration.application.PreRegistrationService;
 import me.donas.boost.domain.preregistration.domain.PreRegistration;
-import me.donas.boost.domain.preregistration.dto.PreRegistrationDto;
+import me.donas.boost.domain.preregistration.dto.PreRegistrationRequest;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,8 +23,8 @@ public class PreRegistrationController {
 	private final PreRegistrationService preRegistrationService;
 
 	@PostMapping("/pre-registrations")
-	public ResponseEntity<Void> register(@RequestBody PreRegistrationDto preRegistrationDto) {
-		PreRegistration register = preRegistrationService.register(preRegistrationDto);
+	public ResponseEntity<Void> register(@RequestBody @Valid PreRegistrationRequest request) {
+		PreRegistration register = preRegistrationService.register(request);
 
 		URI location = ServletUriComponentsBuilder
 			.fromCurrentContextPath().path("/pre-registrations/{id}")
