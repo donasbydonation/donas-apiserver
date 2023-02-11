547b0eeba64a81450b69e2ab4d433afbab083aa8
SeoSeunghun <proto_seo@naver.com>
2023-02-11 22:59:50 +0900

    [feat][#3] 로그인 회원가입 관련 Security 설정 구현

---
+++ src/main/java/me/donas/boost/global/config/SecurityAppConfig.java
@@ -0,0 +1,44 @@
+package me.donas.boost.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import lombok.RequiredArgsConstructor;
+import me.donas.boost.domain.user.repository.UserRepository;
+import me.donas.boost.global.security.UserDetailsServiceImpl;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityAppConfig {
+	private final UserRepository userRepository;
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new UserDetailsServiceImpl(userRepository);
+	}
+
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
+		return authProvider;
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
+	}
+}
--- src/main/java/me/donas/boost/global/config/SecurityConfig.java
+++ src/main/java/me/donas/boost/global/config/SecurityConfig.java
@@ -1,19 +1,31 @@
 package me.donas.boost.global.config;
 
+import static me.donas.boost.domain.user.exception.UserErrorCode.*;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.donas.boost.global.security.JwtAuthenticationFilter;
+import me.donas.boost.global.security.JwtExceptionFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+	private final AuthenticationProvider authenticationProvider;
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	private final JwtExceptionFilter jwtExceptionFilter;
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
 		http
 			.cors().and()
 			.formLogin().disable()
@@ -22,8 +34,17 @@ public class SecurityConfig {
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and()
 			.authorizeHttpRequests(authorizeRequest -> {
-				authorizeRequest.requestMatchers("/api/v1/pre-registrations").permitAll();
-			});
+				authorizeRequest.requestMatchers("/api/v1/pre-registrations").permitAll()
+					.requestMatchers("/api/v1/confirm-email-certification").permitAll()
+					.requestMatchers("/api/v1/issue-email-certification").permitAll()
+					.requestMatchers("/api/v1/signup").permitAll()
+					.requestMatchers("/api/v1/login").permitAll()
+					.requestMatchers("/api/v1/test-user").hasRole("USER")
+					.requestMatchers("/api/v1/test-admin").hasRole("ADMIN")
+					.anyRequest().authenticated();
+			}).authenticationProvider(authenticationProvider)
+			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+			.addFilterBefore(jwtExceptionFilter, JwtAuthenticationFilter.class);
 
 		return http.build();
 	}
