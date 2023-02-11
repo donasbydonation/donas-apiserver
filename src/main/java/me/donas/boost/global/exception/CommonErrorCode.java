a62cac8fd309334624a946e5253eea52b3ae4343
SeoSeunghun <proto_seo@naver.com>
2023-02-11 23:00:54 +0900

    [feat][#3] AuthenticationException 관련 예외처리 추가

--- src/main/java/me/donas/boost/global/exception/CommonErrorCode.java
+++ src/main/java/me/donas/boost/global/exception/CommonErrorCode.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 public enum CommonErrorCode implements ErrorCode {
 	INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "입력 형식을 확인해주세요."),
 	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에 오류가 발생했습니다."),
+	UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증에 실패했습니다."),
 	DUPLICATE_EMAIL(HttpStatus.CONFLICT, "이메일이 중복되었습니다.");
 
 	private final HttpStatus httpStatus;
