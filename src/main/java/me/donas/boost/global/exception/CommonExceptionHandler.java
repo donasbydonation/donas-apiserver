b287184f24fabc68138bf9c0c688b5440824daeb
SeoSeunghun <proto_seo@naver.com>
2023-02-11 23:00:54 +0900

    [feat][#3] AuthenticationException 관련 예외처리 추가

--- src/main/java/me/donas/boost/global/exception/CommonExceptionHandler.java
+++ src/main/java/me/donas/boost/global/exception/CommonExceptionHandler.java
@@ -6,39 +6,48 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import me.donas.boost.global.dto.ErrorResponse;
-import me.donas.boost.global.dto.ErrorResponse.ValidationError;
+import lombok.extern.slf4j.Slf4j;
+import me.donas.boost.global.dto.CustomErrorResponse;
+import me.donas.boost.global.dto.CustomErrorResponse.ValidationError;
 
+@Slf4j
 @RestControllerAdvice
 public class CommonExceptionHandler {
 
 	@ExceptionHandler(CommonException.class)
-	public ResponseEntity<ErrorResponse> handleCommonException(CommonException e) {
+	public ResponseEntity<CustomErrorResponse> handleCommonException(CommonException e) {
 		ErrorCode errorCode = e.errorCode();
-		return ResponseEntity.status(errorCode.httpStatus()).body(ErrorResponse.of(errorCode));
+		return ResponseEntity.status(errorCode.httpStatus()).body(CustomErrorResponse.of(errorCode));
 	}
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+	public ResponseEntity<CustomErrorResponse> handleMethodArgumentNotValidException(
+		MethodArgumentNotValidException e) {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(makeErrorResponse(e));
 	}
 
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<ErrorResponse> handleException(Exception e) {
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ErrorResponse.of(INTERNAL_SERVER_ERROR));
+	@ExceptionHandler(AuthenticationException.class)
+	public ResponseEntity<CustomErrorResponse> handleAuthenticationException(AuthenticationException e) {
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(CustomErrorResponse.of(UNAUTHORIZED));
 	}
 
-	private ErrorResponse makeErrorResponse(MethodArgumentNotValidException e) {
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<CustomErrorResponse> handleException(Exception e) {
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(CustomErrorResponse.of(INTERNAL_SERVER_ERROR));
+	}
+
+	private CustomErrorResponse makeErrorResponse(MethodArgumentNotValidException e) {
 		List<ValidationError> validationErrorList = e.getBindingResult()
 			.getFieldErrors()
 			.stream()
 			.map(ValidationError::of)
 			.toList();
 
-		return ErrorResponse.of(INVALID_PARAMETER, validationErrorList);
+		return CustomErrorResponse.of(INVALID_PARAMETER, validationErrorList);
 	}
 }
