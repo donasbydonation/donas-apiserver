6b6e7a427472c5eea0f34c40398043dfb701c1fa
SeoSeunghun <proto_seo@naver.com>
2023-02-12 23:17:10 +0900

    [feat][#3] JWT 예외, 인증 필터 추가

---
+++ src/main/java/me/donas/boost/global/security/JwtAuthenticationFilter.java
@@ -0,0 +1,56 @@
+package me.donas.boost.global.security;
+
+import static me.donas.boost.domain.user.exception.UserErrorCode.*;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+	private final JwtService jwtService;
+	private final UserDetailsService userDetailsService;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws ServletException, IOException {
+		String authHeader = request.getHeader("Authorization");
+
+		if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+		String jwt = authHeader.substring(7);
+		if (jwtService.isTokenValid(jwt)) {
+			String username = jwtService.extractUsername(jwt);
+			UserDetails userPrincipal = this.userDetailsService.loadUserByUsername(username);
+			SecurityContext context = SecurityContextHolder.createEmptyContext();
+			UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+				userPrincipal,
+				null,
+				userPrincipal.getAuthorities()
+			);
+			authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+			context.setAuthentication(authToken);
+			SecurityContextHolder.setContext(context);
+		}
+		filterChain.doFilter(request, response);
+	}
+
+}
