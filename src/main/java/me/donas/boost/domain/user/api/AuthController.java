124c03785a36ce5722315434d057631d0330690c
SeoSeunghun <proto_seo@naver.com>
2023-02-12 23:26:23 +0900

    [feat][#3] 회원가입, 로그인 관련 API 구현

---
+++ src/main/java/me/donas/boost/domain/user/api/AuthController.java
@@ -0,0 +1,55 @@
+package me.donas.boost.domain.user.api;
+
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import me.donas.boost.domain.user.application.AuthService;
+import me.donas.boost.domain.user.dto.LoginRequest;
+import me.donas.boost.domain.user.dto.LogoutRequest;
+import me.donas.boost.domain.user.dto.RefreshRequest;
+import me.donas.boost.domain.user.dto.TokenResponse;
+import me.donas.boost.domain.user.dto.SignupRequest;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class AuthController {
+	private final AuthService authService;
+
+	@PostMapping("/signup")
+	public ResponseEntity<Void> signup(@RequestBody @Valid SignupRequest request) {
+		Long id = authService.register(request);
+
+		URI location = ServletUriComponentsBuilder
+			.fromCurrentContextPath().path("/users/{id}")
+			.buildAndExpand(id).toUri();
+
+		return ResponseEntity.created(location).build();
+	}
+
+	@PostMapping("/login")
+	public ResponseEntity<TokenResponse> login(@RequestBody @Valid LoginRequest request) {
+		TokenResponse response = authService.login(request);
+		return ResponseEntity.ok(response);
+	}
+
+	@PostMapping("/logout")
+	public ResponseEntity<Void> logout(@RequestBody @Valid LogoutRequest request) {
+		authService.logout(request);
+		return ResponseEntity.ok().build();
+	}
+
+	@PostMapping("/refresh")
+	public ResponseEntity<TokenResponse> refresh(@RequestBody @Valid RefreshRequest request) {
+		TokenResponse response = authService.refresh(request);
+		return ResponseEntity.ok(response);
+	}
+}
