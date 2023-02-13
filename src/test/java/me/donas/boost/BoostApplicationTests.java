1cdf8af6a07f2b9e25a6d547657b611a1583fa40
SeoSeunghun <proto_seo@naver.com>
2023-02-13 15:05:23 +0900

    [test][#3] Test code 빌드 관련 오류 수정

--- src/test/java/me/donas/boost/BoostApplicationTests.java
+++ src/test/java/me/donas/boost/BoostApplicationTests.java
@@ -2,6 +2,7 @@ package me.donas.boost;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootTest
 class BoostApplicationTests {
