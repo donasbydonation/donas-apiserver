bfe7651a25431bda1036b95055cac5e5b2c1fba7
SeoSeunghun <proto_seo@naver.com>
2023-02-18 21:14:30 +0900

    [feat][#12] User 관련 수정 API 구현

--- src/main/java/me/donas/boost/domain/user/api/UserController.java
+++ src/main/java/me/donas/boost/domain/user/api/UserController.java
@@ -1,9 +1,10 @@
 package me.donas.boost.domain.user.api;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import me.donas.boost.domain.user.application.UserService;
+import me.donas.boost.domain.user.domain.UserPrincipal;
 import me.donas.boost.domain.user.dto.UpdatePasswordRequest;
+import me.donas.boost.domain.user.dto.UpdateUserInfoRequest;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,15 +23,35 @@ import me.donas.boost.domain.user.dto.UpdatePasswordRequest;
 public class UserController {
 	private final UserService userService;
 
-	@PutMapping("/{id}/password")
-	public ResponseEntity<Void> updatePassword(@PathVariable Long id, @RequestBody @Valid UpdatePasswordRequest request) {
-		userService.updatePassword(id, request);
+	@PutMapping("/{username}/password")
+	@PreAuthorize("hasRole('USER')")
+	public ResponseEntity<Void> updatePassword(
+		@AuthenticationPrincipal UserPrincipal userPrincipal,
+		@PathVariable String username,
+		@RequestBody @Valid UpdatePasswordRequest request
+	) {
+		userService.updatePassword(userPrincipal, username, request);
 		return ResponseEntity.ok().build();
 	}
 
-	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> withdraw(@PathVariable Long id) {
-		userService.withdraw(id);
+	@PutMapping("/{username}/information")
+	@PreAuthorize("hasRole('USER')")
+	public ResponseEntity<Void> updateUserInformation(
+		@AuthenticationPrincipal UserPrincipal userPrincipal,
+		@PathVariable String username,
+		@RequestBody UpdateUserInfoRequest request
+	) {
+		userService.updateUserInformation(userPrincipal, username, request);
+		return ResponseEntity.ok().build();
+	}
+
+	@DeleteMapping("/{username}")
+	@PreAuthorize("hasRole('USER')")
+	public ResponseEntity<Void> withdraw(
+		@AuthenticationPrincipal UserPrincipal userPrincipal,
+		@PathVariable String username
+	) {
+		userService.withdraw(userPrincipal, username);
 		return ResponseEntity.ok().build();
 	}
 }
