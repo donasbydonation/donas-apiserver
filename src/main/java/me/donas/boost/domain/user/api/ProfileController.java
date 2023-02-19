9c9b46bbd10b1cd92d6dc44dacd8ff3c1fc914d9
SeoSeunghun <proto_seo@naver.com>
2023-02-19 21:13:53 +0900

    [feat][#12] Profile 반환 기능 구현

--- src/main/java/me/donas/boost/domain/user/api/ProfileController.java
+++ src/main/java/me/donas/boost/domain/user/api/ProfileController.java
@@ -1,15 +1,10 @@
 package me.donas.boost.domain.user.api;
 
-import java.io.IOException;
-import java.io.InputStream;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -22,7 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 import me.donas.boost.domain.user.application.ProfileService;
 import me.donas.boost.domain.user.domain.UserPrincipal;
 import me.donas.boost.domain.user.dto.ProfileUpdateRequest;
-import me.donas.boost.domain.user.dto.ProfileUpdateResponse;
+import me.donas.boost.domain.user.dto.ProfileResponse;
 
 @Slf4j
 @RestController
@@ -31,14 +26,20 @@ import me.donas.boost.domain.user.dto.ProfileUpdateResponse;
 public class ProfileController {
 	private final ProfileService profileService;
 
+	@GetMapping("/profile/{username}")
+	public ResponseEntity<ProfileResponse> getProfile(@PathVariable String username) {
+		ProfileResponse response = profileService.getProfile(username);
+		return ResponseEntity.ok(response);
+	}
+
 	@PutMapping("/profile")
 	@PreAuthorize("hasRole('USER')")
-	public ResponseEntity<ProfileUpdateResponse> updateProfile(
+	public ResponseEntity<ProfileResponse> updateProfile(
 		@AuthenticationPrincipal UserPrincipal userPrincipal,
 		@Valid @RequestPart(value = "profileData") ProfileUpdateRequest request,
 		@RequestPart(value = "image", required = false) MultipartFile file
 	) {
-		ProfileUpdateResponse response = profileService.updateProfile(userPrincipal, request, file);
+		ProfileResponse response = profileService.updateProfile(userPrincipal, request, file);
 		return ResponseEntity.ok(response);
 	}
 }
