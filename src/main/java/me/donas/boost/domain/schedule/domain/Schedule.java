56b2ed356394912cc896e53333aea6b32eee8450
SeoSeunghun <proto_seo@naver.com>
2023-04-02 23:50:19 +0900

    [feat][#21] 방송 스케줄을 저장하기 위한 domain 생성

---
+++ src/main/java/me/donas/boost/domain/schedule/domain/Schedule.java
@@ -0,0 +1,35 @@
+package me.donas.boost.domain.schedule.domain;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Schedule {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "creator_id")
+	CreatorInfo creator;
+
+	String title;
+
+	@Lob
+	String description;
+
+	LocalDateTime scheduledTime;
+}
