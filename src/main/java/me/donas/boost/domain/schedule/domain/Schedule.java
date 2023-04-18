5716283f2125554a6e27f930e5d610764008d334
SeoSeunghun <proto_seo@naver.com>
2023-04-18 15:50:41 +0900

    [feat][#21] Schedule, CreatorInfo, Platform domain 생성

--- src/main/java/me/donas/boost/domain/schedule/domain/Schedule.java
+++ src/main/java/me/donas/boost/domain/schedule/domain/Schedule.java
@@ -2,6 +2,7 @@ package me.donas.boost.domain.schedule.domain;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -11,8 +12,10 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import me.donas.boost.domain.schedule.dto.ScheduleUpdateRequest;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -20,16 +23,41 @@ import lombok.NoArgsConstructor;
 public class Schedule {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "schedule_id")
 	Long id;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "creator_id")
-	CreatorInfo creator;
+	@JoinColumn(name = "creator_info_id")
+	CreatorInfo creatorInfo;
 
 	String title;
+
+	String bannerImage;
 
 	@Lob
 	String description;
 
 	LocalDateTime scheduledTime;
+
+	@Builder
+	public Schedule(CreatorInfo creatorInfo, String title, String bannerImage, String description,
+		LocalDateTime scheduledTime) {
+		this.creatorInfo = creatorInfo;
+		this.title = title;
+		this.bannerImage = bannerImage;
+		this.description = description;
+		this.scheduledTime = scheduledTime;
+	}
+
+	public void update(CreatorInfo creatorInfo, String bannerImage, ScheduleUpdateRequest request) {
+		this.creatorInfo = creatorInfo;
+		this.bannerImage = bannerImage;
+		this.title = request.title();
+		this.description = request.description();
+		this.scheduledTime = request.scheduledTime();
+	}
+
+	public void remove() {
+		this.creatorInfo = null;
+	}
 }
