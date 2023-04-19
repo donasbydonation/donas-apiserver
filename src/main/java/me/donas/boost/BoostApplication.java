8f1afb262f1d058ac180809e83bdda01aa23c44c
SeoSeunghun <proto_seo@naver.com>
2023-04-19 13:48:28 +0900

    [feat][#24] bean이 다 생성 되면, admin 유저 생성

--- src/main/java/me/donas/boost/BoostApplication.java
+++ src/main/java/me/donas/boost/BoostApplication.java
@@ -2,12 +2,28 @@ package me.donas.boost;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import me.donas.boost.domain.common.admin.AdminUser;
+import me.donas.boost.domain.user.repository.UserRepository;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class BoostApplication {
+	private final UserRepository userRepository;
+	private final AdminUser adminUser;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoostApplication.class, args);
 	}
 
+	@PostConstruct
+	@Transactional
+	public void createAdminUser() {
+		if (!userRepository.existsByUsername(adminUser.getUsername())) {
+			userRepository.save(adminUser.toEntity());
+		}
+	}
 }
