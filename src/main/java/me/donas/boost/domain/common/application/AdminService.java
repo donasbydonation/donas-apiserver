2beb87f62ae796f69a922c56f3c03906752ce0a0
SeoSeunghun <proto_seo@naver.com>
2023-04-19 13:47:59 +0900

    [feat][#24] admin login 과정 수정 및 admin refresh 로직 추가

--- src/main/java/me/donas/boost/domain/common/application/AdminService.java
+++ src/main/java/me/donas/boost/domain/common/application/AdminService.java
@@ -1,4 +1,4 @@
-package me.donas.boost.domain.schedule.application;
+package me.donas.boost.domain.common.application;
 
 import static me.donas.boost.domain.user.exception.UserErrorCode.*;
 
@@ -6,9 +6,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import me.donas.boost.domain.common.admin.AdminUser;
 import me.donas.boost.domain.user.domain.User;
-import me.donas.boost.domain.user.domain.UserPrincipal;
 import me.donas.boost.domain.user.dto.LoginRequest;
+import me.donas.boost.domain.user.dto.RefreshRequest;
 import me.donas.boost.domain.user.dto.TokenResponse;
 import me.donas.boost.domain.user.exception.UserException;
 import me.donas.boost.domain.user.repository.UserRepository;
@@ -17,20 +18,38 @@ import me.donas.boost.global.security.JwtService;
 @Service
 @RequiredArgsConstructor
 public class AdminService {
+	private final AdminUser adminUser;
 	private final UserRepository userRepository;
 	private final JwtService jwtService;
 
-	@Transactional(readOnly = true)
+	@Transactional
 	public TokenResponse adminLogin(LoginRequest request) {
-		User user = userRepository.findByUsername(request.username())
-			.orElseThrow(() -> new UserException(NOTFOUND));
-
-		if (user.getPassword().equals(request.password())) {
-			String accessToken = jwtService.generateToken(UserPrincipal.create(user));
+		if (isValidAdmin(request)) {
+			User user = userRepository.findByUsername(request.username())
+				.orElseThrow(() -> new UserException(NOTFOUND));
+			String accessToken = jwtService.generateAdminToken(adminUser);
 			String refreshToken = user.login();
-
 			return new TokenResponse(accessToken, refreshToken);
 		}
 		throw new UserException(INVALID_PASSWORD);
+	}
+
+	private boolean isValidAdmin(LoginRequest request) {
+		return adminUser.getUsername().equals(request.username()) && adminUser.getPassword().equals(request.password());
+	}
+
+	@Transactional
+	public TokenResponse adminRefresh(RefreshRequest request) {
+		String username = request.username();
+		String refreshToken = request.refreshToken();
+		User user = userRepository.findByUsername(username).orElseThrow(
+			() -> new UserException(NOTFOUND)
+		);
+		if (!user.getRefreshToken().isValid(refreshToken)) {
+			throw new UserException(EXPIRED_REFRESH_TOKEN);
+		}
+		String accessToken = jwtService.generateAdminToken(adminUser);
+		String updateRefreshToken = user.updateRefreshToken();
+		return new TokenResponse(accessToken, updateRefreshToken);
 	}
 }
