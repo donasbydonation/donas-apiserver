ee25d5fd2301770ebf2b2cef049642e382005e9d
SeoSeunghun <proto_seo@naver.com>
2023-04-19 13:38:27 +0900

    [chore][#24] configuration processor 의존성 추가

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-insert into user(agree_promotion, is_withdraw, password, username, role)
-select 0, 0, 'admin', 'Zv5wuh01Y37RQ', 'ROLE_ADMIN'
-    where not exists (
-    select username from user where username = 'admin'
-);
