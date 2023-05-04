7d22ee5c139d17c5741f002b0b264e3cd1ec3bee
SeoSeunghun <proto_seo@naver.com>
2023-05-04 17:57:18 +0900

    [feat][#26] 메인 페이지 스케쥴 호출 API 생성

--- src/main/java/me/donas/boost/domain/schedule/api/ScheduleController.java
+++ src/main/java/me/donas/boost/domain/schedule/api/ScheduleController.java
@@ -1,8 +1,9 @@
 package me.donas.boost.domain.schedule.api;
 
 import java.net.URI;
-import java.util.List;
+import java.time.ZonedDateTime;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -19,12 +19,14 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import me.donas.boost.domain.schedule.application.ScheduleService;
+import me.donas.boost.domain.schedule.domain.PlatformProvider;
 import me.donas.boost.domain.schedule.dto.ScheduleRequest;
-import me.donas.boost.domain.schedule.dto.ScheduleResponse;
 import me.donas.boost.domain.schedule.dto.ScheduleUpdateRequest;
 import me.donas.boost.domain.schedule.dto.SchedulesResponse;
+import me.donas.boost.domain.schedule.dto.SchedulesTotalResponse;
 
 @RestController
 @RequiredArgsConstructor
@@ -70,5 +72,16 @@ public class ScheduleController {
 	@PreAuthorize("hasRole('ADMIN')")
 	public ResponseEntity<Long> deleteSchedule(@PathVariable Long id) {
 		return ResponseEntity.ok(scheduleService.deleteSchedule(id));
+	}
+
+	@GetMapping("/schedules/list")
+	public ResponseEntity<SchedulesTotalResponse> readSchedules(
+		@RequestParam @DateTimeFormat(iso= DateTimeFormat.ISO.DATE_TIME) ZonedDateTime time,
+		@RequestParam(defaultValue = "0") int day,
+		@RequestParam(defaultValue = "TOTAL") PlatformProvider provider,
+		@RequestParam(defaultValue = "0") int page,
+		@RequestParam(defaultValue = "5") int size
+	) {
+		return ResponseEntity.ok(scheduleService.readSchedules(time, day, provider, page, size));
 	}
 }
