68d9f63c66b36e8f339fd36b5967126069fd000f
SeoSeunghun <proto_seo@naver.com>
2023-05-04 17:57:29 +0900

    [feat][#26] 메인 페이지 기능 생성

--- src/main/java/me/donas/boost/domain/schedule/application/ScheduleService.java
+++ src/main/java/me/donas/boost/domain/schedule/application/ScheduleService.java
@@ -3,7 +3,17 @@ package me.donas.boost.domain.schedule.application;
 import static me.donas.boost.domain.schedule.exception.CreatorInfoErrorCode.*;
 import static me.donas.boost.domain.schedule.exception.ScheduleErrorCode.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.zone.ZoneOffsetTransition;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -12,18 +22,19 @@ import org.springframework.web.multipart.MultipartFile;
 
 import lombok.RequiredArgsConstructor;
 import me.donas.boost.domain.schedule.domain.CreatorInfo;
+import me.donas.boost.domain.schedule.domain.PlatformProvider;
 import me.donas.boost.domain.schedule.domain.Schedule;
-import me.donas.boost.domain.schedule.dto.CreatorInfoRequest;
+import me.donas.boost.domain.schedule.dto.ScheduleQueryResponse;
+import me.donas.boost.domain.schedule.dto.ScheduleQueryResponses;
 import me.donas.boost.domain.schedule.dto.ScheduleRequest;
 import me.donas.boost.domain.schedule.dto.ScheduleResponse;
 import me.donas.boost.domain.schedule.dto.ScheduleUpdateRequest;
 import me.donas.boost.domain.schedule.dto.SchedulesResponse;
-import me.donas.boost.domain.schedule.exception.CreatorInfoErrorCode;
+import me.donas.boost.domain.schedule.dto.SchedulesTotalResponse;
 import me.donas.boost.domain.schedule.exception.CreatorInfoException;
-import me.donas.boost.domain.schedule.exception.ScheduleErrorCode;
 import me.donas.boost.domain.schedule.exception.ScheduleException;
 import me.donas.boost.domain.schedule.repository.CreatorInfoRepository;
-import me.donas.boost.domain.schedule.repository.PlatformRepository;
+import me.donas.boost.domain.schedule.repository.ScheduleQueryRepository;
 import me.donas.boost.domain.schedule.repository.ScheduleRepository;
 import me.donas.boost.global.utils.S3UploadUtils;
 
@@ -31,6 +42,7 @@ import me.donas.boost.global.utils.S3UploadUtils;
 @RequiredArgsConstructor
 public class ScheduleService {
 	private final ScheduleRepository scheduleRepository;
+	private final ScheduleQueryRepository scheduleQueryRepository;
 	private final CreatorInfoRepository creatorInfoRepository;
 	private final S3UploadUtils s3UploadUtils;
 
@@ -53,6 +65,54 @@ public class ScheduleService {
 		long totalCount = scheduleRepository.count();
 		long totalPage = (totalCount % size == 0) ? totalCount / size : (totalCount / size) + 1;
 		return SchedulesResponse.of(totalPage, schedules);
+	}
+
+	@Transactional(readOnly = true)
+	public SchedulesTotalResponse readSchedules(ZonedDateTime time, int day, PlatformProvider provider, int page,
+		int size) {
+		LocalDateTime date = LocalDateTime.of(time.toLocalDate(), LocalTime.MIDNIGHT);
+		String timeOffsetId = time.getOffset().getId();
+		ZoneOffsetTransition transition = ZoneOffsetTransition.of(date, ZoneOffset.of(timeOffsetId), ZoneOffset.UTC);
+		LocalDateTime start = transition.getDateTimeAfter();
+		LocalDateTime end = start.plusDays(1).minusMinutes(1);
+
+		List<ScheduleQueryResponse> schedules = scheduleQueryRepository.findAllByScheduledTimeBetween(day, start, end);
+		if (PlatformProvider.TOTAL.equals(provider)) {
+			return organizeByTotal(page, size, time.withZoneSameInstant(ZoneOffset.UTC), schedules);
+		} else {
+			return organizeByPlatform(provider, schedules);
+		}
+	}
+
+	private SchedulesTotalResponse organizeByTotal(int page, int size, ZonedDateTime now,
+		List<ScheduleQueryResponse> schedules) {
+		Map<PlatformProvider, List<ScheduleQueryResponse>> map = new HashMap<>();
+		Map<PlatformProvider, ScheduleQueryResponses> converter = new HashMap<>();
+
+		for (ScheduleQueryResponse r : schedules) {
+			PlatformProvider p = r.getProvider();
+			List<ScheduleQueryResponse> l = map.getOrDefault(p, new ArrayList<>());
+			l.add(r);
+			map.put(p, l);
+		}
+
+		for (Map.Entry<PlatformProvider, List<ScheduleQueryResponse>> entry : map.entrySet()) {
+			List<ScheduleQueryResponse> l = entry.getValue();
+			long totalPage = l.size() % size == 0 ? l.size() / size : (l.size() / size) + 1;
+			long nowSize = l.stream().filter(r -> r.getScheduledTime().isBefore(now)).toList().size();
+			long recommendPage = nowSize % size == 0 ? nowSize / size : (nowSize / size) + 1;
+			List<ScheduleQueryResponse> s = l.stream().skip((long)page * size).limit(size).toList();
+			converter.put(entry.getKey(), ScheduleQueryResponses.of(totalPage, recommendPage, s));
+		}
+		return SchedulesTotalResponse.of(converter);
+	}
+
+	private SchedulesTotalResponse organizeByPlatform(PlatformProvider provider,
+		List<ScheduleQueryResponse> schedules) {
+		Map<PlatformProvider, ScheduleQueryResponses> converter = new HashMap<>();
+		converter.put(provider,
+			ScheduleQueryResponses.of(1, 1, schedules.stream().filter(r -> r.getProvider().equals(provider)).toList()));
+		return SchedulesTotalResponse.of(converter);
 	}
 
 	@Transactional
