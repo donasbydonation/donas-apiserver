f9bae1ed94bc0793c4342a9a040e71cbd4e990d8
SeoSeunghun <proto_seo@naver.com>
2023-05-21 23:51:06 +0900

    refactor: 시간 정보 관련 변수 변경 및 메서드 추출

--- src/main/java/me/donas/boost/domain/schedule/repository/ScheduleQueryRepository.java
+++ src/main/java/me/donas/boost/domain/schedule/repository/ScheduleQueryRepository.java
@@ -5,9 +5,13 @@ import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
+import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
+import me.donas.boost.domain.schedule.application.SearchBetweenTime;
+import me.donas.boost.domain.schedule.domain.PlatformProvider;
 import me.donas.boost.domain.schedule.domain.QCreatorInfo;
 import me.donas.boost.domain.schedule.domain.QPlatform;
 import me.donas.boost.domain.schedule.domain.QSchedule;
@@ -19,16 +23,20 @@ import me.donas.boost.domain.schedule.dto.ScheduleQueryResponse;
 public class ScheduleQueryRepository {
 	private final JPAQueryFactory queryFactory;
 
-	public List<ScheduleQueryResponse> findAllByScheduledTimeBetween(int day, LocalDateTime start, LocalDateTime end) {
+	public List<ScheduleQueryResponse> findAllByScheduledTimeBetweenAndPlatformProvider(int day,
+		SearchBetweenTime searchBetweenTime, PlatformProvider provider) {
 		QSchedule schedule = QSchedule.schedule;
 		QCreatorInfo creatorInfo = QCreatorInfo.creatorInfo;
 		QPlatform platform = QPlatform.platform;
+		LocalDateTime start = searchBetweenTime.start();
+		LocalDateTime end = searchBetweenTime.end();
 
 		return queryFactory.select(new QScheduleQueryResponse(
 				creatorInfo.name,
 				creatorInfo.profileImage,
 				platform.provider,
 				platform.broadcastLink,
+				schedule.id,
 				schedule.title,
 				schedule.bannerImage,
 				schedule.description,
@@ -37,8 +45,19 @@ public class ScheduleQueryRepository {
 			.from(schedule)
 			.leftJoin(creatorInfo).on(schedule.creatorInfo.id.eq(creatorInfo.id))
 			.leftJoin(platform).on(schedule.creatorInfo.id.eq(platform.creatorInfo.id))
-			.where(schedule.scheduledTime.between(start.plusDays(day), end.plusDays(day)))
+			.where(
+				scheduledTimeBetween(day, start, end),
+				platformProviderEq(provider)
+			)
 			.orderBy(schedule.scheduledTime.asc())
 			.fetch();
+	}
+
+	private BooleanExpression scheduledTimeBetween(int day, LocalDateTime start, LocalDateTime end) {
+		return QSchedule.schedule.scheduledTime.between(start.plusDays(day), end.plusDays(day));
+	}
+
+	private BooleanExpression platformProviderEq(PlatformProvider provider) {
+		return (PlatformProvider.TOTAL.equals(provider)) ? null : QPlatform.platform.provider.eq(provider);
 	}
 }
