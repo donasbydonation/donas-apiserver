43c144aeeaef2abd295f45be1caad2ddd65e806f
SeoSeunghun <proto_seo@naver.com>
2023-06-12 14:57:09 +0900

    [feat][#37] CreatorInfo 호출 시 Platform 정보를 같이 가져오는 메서드 생성

--- src/main/java/me/donas/boost/domain/schedule/repository/CreatorInfoRepository.java
+++ src/main/java/me/donas/boost/domain/schedule/repository/CreatorInfoRepository.java
@@ -3,6 +3,7 @@ package me.donas.boost.domain.schedule.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import me.donas.boost.domain.schedule.domain.CreatorInfo;
@@ -11,4 +12,7 @@ public interface CreatorInfoRepository extends JpaRepository<CreatorInfo, Long> 
 	boolean existsByName(String name);
 	Optional<CreatorInfo> findByName(String name);
 	List<CreatorInfo> findAllByNameContainingIgnoreCase(String name);
+
+	@EntityGraph(attributePaths = "platforms")
+	Optional<CreatorInfo> findCreatorInfoWithPlatformsById(Long id);
 }
