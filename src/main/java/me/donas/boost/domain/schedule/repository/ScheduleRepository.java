2635de26b3f69a88603e60a64c45f5d9a899f13e
SeoSeunghun <proto_seo@naver.com>
2023-06-12 14:57:40 +0900

    [feat][#37] 입력받은 시간 이후의 해당 크리에이터의 스케줄을 반환하는 메서드 생성

--- src/main/java/me/donas/boost/domain/schedule/repository/ScheduleRepository.java
+++ src/main/java/me/donas/boost/domain/schedule/repository/ScheduleRepository.java
@@ -1,8 +1,15 @@
 package me.donas.boost.domain.schedule.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import me.donas.boost.domain.schedule.domain.CreatorInfo;
 import me.donas.boost.domain.schedule.domain.Schedule;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+	List<Schedule> findAllByCreatorInfoAndScheduledTimeIsGreaterThanEqual(CreatorInfo creatorInfo, LocalDateTime scheduledTime, Sort sort);
 }
