d5531a04c4c355cca133e255fe35672aad439041
SeoSeunghun <proto_seo@naver.com>
2023-06-12 14:54:57 +0900

    [feat][#37] 편성표 호출 시 정보에 크리에이터의 ID를 포함하도록 추가

--- src/main/java/me/donas/boost/domain/schedule/dto/ScheduleResultResponse.java
+++ src/main/java/me/donas/boost/domain/schedule/dto/ScheduleResultResponse.java
@@ -4,6 +4,7 @@ import java.time.ZonedDateTime;
 import java.util.List;
 
 public record ScheduleResultResponse(
+	Long creatorId,
 	String creatorName,
 	String profileImage,
 	List<PlatformResultResponse> platforms,
@@ -15,6 +16,7 @@ public record ScheduleResultResponse(
 	public static ScheduleResultResponse of(ScheduleQueryResponse scheduleQueryResponse,
 		List<PlatformResultResponse> platformResultResponses) {
 		return new ScheduleResultResponse(
+			scheduleQueryResponse.getCreatorId(),
 			scheduleQueryResponse.getCreatorName(),
 			scheduleQueryResponse.getProfileImage(),
 			platformResultResponses,
